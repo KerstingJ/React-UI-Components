f8dcb7fddae8f7ddb3326084bfb69c7d4e5e0796
KerstingJ <Kersting.Josh@Gmail.com>
2019-03-19 16:34:20 -0400

    displays calculator

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = props => {
+    return (
+        <div className={`btn actBtn ${props.size}`}>{props.value}</div>
+    );
+}
+
+export default ActionButton;
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = props => {
+    return (
+        <div className={`btn numBtn ${props.size}`}>{props.value}</div>
+    );
+}
+
+export default NumberButton;
